a5c36c46e31474f61db36bb1fff0b1b5c0de5445
iamkartikeysingh <kartikeysingh440@gmail.com>
2021-08-29 19:22:41 +0530

    Encode or Decode letters

---
+++ Caesar_Cipher.py
@@ -0,0 +1,30 @@
+from art import logo
+print(logo)
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(start_text, shift_amount, cipher_direction):
+    end_text = ""
+    if cipher_direction == "decode":
+            shift_amount *= -1
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
+    print(f"Your {cipher_direction}d text is {end_text}.")
+
+end_of_caesar_cipher = False
+while not end_of_caesar_cipher:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift = shift%26
+    caesar(start_text=text, shift_amount= shift, cipher_direction=direction)   
+    choice = input("Do you want to try again? ").lower()
+    if choice == "no":
+        print("Game Over")
+        end_of_caesar_cipher = True 
+
